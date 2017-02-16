cb5268faf9920f724dd74ac6ae05d528a73b22e4
rchinta <rchinta@cs.odu.edu>
2017-02-16 15:04:04 -0500

    new db dump

--- hacku5.sql
+++ hacku5.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2017 at 07:32 PM
+-- Generation Time: Feb 16, 2017 at 09:02 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -31,21 +31,8 @@ USE `hacku5`;
 CREATE TABLE `cu_domain_category` (
   `cid` int(10) NOT NULL,
   `domain_name` varchar(200) DEFAULT NULL,
-  `domain_category` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cu_modes`
---
-
-CREATE TABLE `cu_modes` (
-  `mid` int(10) NOT NULL,
-  `url_id` int(10) DEFAULT NULL,
-  `url` varchar(200) DEFAULT NULL,
-  `url_title` varchar(100) DEFAULT NULL,
-  `creation_date` date DEFAULT NULL
+  `domain_category` varchar(200) DEFAULT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -60,27 +47,6 @@ CREATE TABLE `cu_mode_url` (
   `url` varchar(200) DEFAULT NULL,
   `url_title` varchar(100) DEFAULT NULL,
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cu_user_details`
---
-
-CREATE TABLE `cu_user_details` (
-  `uid` int(10) NOT NULL,
-  `first_name` varchar(100) DEFAULT NULL,
-  `last_name` varchar(100) DEFAULT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  `user_pass` varchar(50) DEFAULT NULL,
-  `user_activity_flag` tinyint(1) DEFAULT NULL,
-  `age` int(10) DEFAULT NULL,
-  `gender` varchar(5) DEFAULT NULL,
-  `dob` date DEFAULT NULL,
-  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_login_date` date DEFAULT NULL,
-  `user_email` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -114,6 +80,31 @@ CREATE TABLE `cu_user_modes` (
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `oauth_provider` varchar(255) NOT NULL,
+  `oauth_uid` varchar(255) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `locale` varchar(10) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `fb_id` varchar(100) DEFAULT NULL,
+  `fb_name` varchar(60) DEFAULT NULL,
+  `user_activity_flag` tinyint(1) DEFAULT NULL,
+  `last_login_date` date NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `modified_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -122,13 +113,8 @@ CREATE TABLE `cu_user_modes` (
 -- Indexes for table `cu_domain_category`
 --
 ALTER TABLE `cu_domain_category`
-  ADD PRIMARY KEY (`cid`);
-
---
--- Indexes for table `cu_modes`
---
-ALTER TABLE `cu_modes`
-  ADD PRIMARY KEY (`mid`);
+  ADD PRIMARY KEY (`cid`),
+  ADD UNIQUE KEY `domain_name_uk` (`domain_name`);
 
 --
 -- Indexes for table `cu_mode_url`
@@ -137,17 +123,11 @@ ALTER TABLE `cu_mode_url`
   ADD PRIMARY KEY (`mid`);
 
 --
--- Indexes for table `cu_user_details`
---
-ALTER TABLE `cu_user_details`
-  ADD PRIMARY KEY (`uid`);
-
---
 -- Indexes for table `cu_user_likes`
 --
 ALTER TABLE `cu_user_likes`
   ADD PRIMARY KEY (`lid`),
-  ADD KEY `cu_user_likes_uid_fk` (`mid`);
+  ADD KEY `cu_user_likes_mid_fk` (`mid`);
 
 --
 -- Indexes for table `cu_user_modes`
@@ -155,6 +135,12 @@ ALTER TABLE `cu_user_likes`
 ALTER TABLE `cu_user_modes`
   ADD PRIMARY KEY (`mid`),
   ADD KEY `cu_user_modes_uid_fk` (`uid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -166,16 +152,6 @@ ALTER TABLE `cu_user_modes`
 ALTER TABLE `cu_domain_category`
   MODIFY `cid` int(10) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `cu_modes`
---
-ALTER TABLE `cu_modes`
-  MODIFY `mid` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `cu_user_details`
---
-ALTER TABLE `cu_user_details`
-  MODIFY `uid` int(10) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `cu_user_likes`
 --
 ALTER TABLE `cu_user_likes`
@@ -185,6 +161,11 @@ ALTER TABLE `cu_user_likes`
 --
 ALTER TABLE `cu_user_modes`
   MODIFY `mid` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -200,13 +181,13 @@ ALTER TABLE `cu_mode_url`
 --
 ALTER TABLE `cu_user_likes`
   ADD CONSTRAINT `cu_user_likes_mid_fk` FOREIGN KEY (`mid`) REFERENCES `cu_user_modes` (`mid`),
-  ADD CONSTRAINT `cu_user_likes_uid_fk` FOREIGN KEY (`mid`) REFERENCES `cu_user_details` (`uid`);
+  ADD CONSTRAINT `cu_user_likes_uid_fk` FOREIGN KEY (`mid`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `cu_user_modes`
 --
 ALTER TABLE `cu_user_modes`
-  ADD CONSTRAINT `cu_user_modes_uid_fk` FOREIGN KEY (`uid`) REFERENCES `cu_user_details` (`uid`);
+  ADD CONSTRAINT `cu_user_modes_uid_fk` FOREIGN KEY (`uid`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
